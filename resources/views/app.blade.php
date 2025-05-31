ad2f337b9e7dfd385eff36cd065a971fe0e91182
phun0555 <phun0555@gmail.com>
2025-05-31 13:56:27 +0700

    -

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <base href="/CurrencyManager/">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Currency Manager</title>
   @viteReactRefresh
